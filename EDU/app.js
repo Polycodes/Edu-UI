c764eb3f7988dff585cae88361f35289c4095e44
Polycarp <100169662+Polycodes@users.noreply.github.com>
2022-05-06 16:31:47 -0600

    Add files via upload

---
+++ EDU/app.js
@@ -0,0 +1,17 @@
+
+window.onload = function(){
+    
+    var mainPage = document.getElementById("mainPage");
+    var loadPage = document.getElementById("loadPage");
+    var duration = 1700;
+    
+    setTimeout(function(){
+    
+     mainPage.style.opacity = "100";
+     loadPage.style.opacity = "0";
+     
+     }, duration);
+
+
+    
+}
